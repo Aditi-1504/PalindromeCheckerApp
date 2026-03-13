cbdbfeef9b33ea3b4cf6565c09586f31b557403e
Aditi-1504 <aditiag150406@gmail.com>
2026-03-13 15:04:08 +0530

    Feat: Seperate UC3 String reverse approach

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,33 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-
-        //UC1
-        System.out.println("=====================================");
-        System.out.println("      Welcome to Palindrome Checker");
-        System.out.println("=====================================");
-        System.out.println("Application Name : PalindromeChecker App");
-        System.out.println("Application Version : 1.0");
-        System.out.println("-------------------------------------");
-        System.out.println("Program started successfully.");
-        System.out.println("Proceeding to next use case...");
-        System.out.println("-------------------------------------");
-
-        //UC2
-        String word = "madam";
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
-        }
-        if (word.equals(reversed)) {
-            System.out.println("The string \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
-        }
-        System.out.println("Program finished.");
-
-        //UC3
         String str = "madam";
-        reversed = "";
+        String reversed = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
             reversed = reversed + str.charAt(i);
