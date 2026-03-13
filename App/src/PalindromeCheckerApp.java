287c353f9779a527441cfedf49d2b146fe965d1c
Aditi-1504 <aditiag150406@gmail.com>
2026-03-13 14:46:57 +0530

    Feat: Seperate UC2: Hard Code Palindrome form main

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,5 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-
-        //UC1
-        System.out.println("=====================================");
-        System.out.println("      Welcome to Palindrome Checker");
-        System.out.println("=====================================");
-        System.out.println("Application Name : PalindromeChecker App");
-        System.out.println("Application Version : 1.0");
-        System.out.println("-------------------------------------");
-        System.out.println("Program started successfully.");
-        System.out.println("Proceeding to next use case...");
-        System.out.println("-------------------------------------");
-
-        //UC2
         String word = "madam";
         String reversed = "";
         for (int i = word.length() - 1; i >= 0; i--) {
